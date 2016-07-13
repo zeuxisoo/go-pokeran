3352f184e2c0e607bf3dfdd07e79b187a9f05357
Zeuxis Lo <seekstudio@gmail.com>
2016-07-13 09:47:50 +0800

    add base code to render empty gui

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+    "log"
 
+    "github.com/jroimartin/gocui"
+)
+
+func main() {
+    // Initial
+    g := gocui.NewGui()
+
+    if err := g.Init(); err != nil {
+        log.Panicln(err)
+    }
+
+    defer g.Close()
+
+    // Layout
+    g.SetLayout(layout)
+
+    // Keybinding
+    if err := keybindings(g); err != nil {
+        log.Panicln(err)
+    }
+
+    // Base color style
+    g.SelBgColor = gocui.ColorGreen
+    g.SelFgColor = gocui.ColorBlack
+    g.Cursor = true
+
+    // Loop
+    if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+        log.Panicln(err)
+    }
+}
+
+func keybindings(g *gocui.Gui) error {
+    return nil
+}
+
+func layout(g *gocui.Gui) error {
+    return nil
 }
